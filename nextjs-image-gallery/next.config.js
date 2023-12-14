95495fb196878dde0b2bc9760383c37068315573
TURP1 <turpicrypto@gmail.com>
2023-12-15 00:51:42 +0200

    fix

--- nextjs-image-gallery/next.config.js
+++ nextjs-image-gallery/next.config.js
@@ -6,7 +6,7 @@ module.exports = {
         protocol: 'https',
         hostname: 'res.cloudinary.com',
         port: '',
-        pathname: `/${process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME}/**`,
+        pathname: `/dv0xwgzeo/**`,
       },
     ],
   },
